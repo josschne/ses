adb5f42cbcdf3fa777eb62a5498aa29d1c10d7e7
Joe Schneider <joe.schneider@lab651.com>
2018-12-19 02:52:11 -0600

    Updated configs

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-docker build build
+docker build config_3.50_sdk_15.0.0 -t josschne/ses:3.50_sdk_15.0.0
+docker build config_3.50_sdk_15.2.0 -t josschne/ses:3.50_sdk_15.2.0
 
 #Test the new image with a smoke test
-GET_VERSION=$(docker run -it josschne/ses /ses/bin/emBuild | grep Release)
+GET_VERSION=$(docker run -it josschne/ses:3.50_sdk_15.0.0 /ses/bin/emBuild | grep Release)
+GET_VERSION=$(docker run -it josschne/ses:3.50_sdk_15.2.0 /ses/bin/emBuild | grep Release)
